89d6e4ad845b5609471bb6b8774844ecf3a8f983
Matt Lebl <mlebl@uvic.ca>
2025-02-11 12:54:10 -0800

    Refactoring

--- apps/backend/src/main/kotlin/ca/uvictrades/trades/matching/MatchingService.kt
+++ apps/backend/src/main/kotlin/ca/uvictrades/trades/matching/MatchingService.kt
@@ -5,22 +5,37 @@ import ca.uvictrades.trades.matching.port.PlaceBuyOrderResult
 import ca.uvictrades.trades.matching.port.SellLimitOrder
 import org.springframework.stereotype.Component
 import java.math.BigDecimal
-import java.util.PriorityQueue
-import kotlin.time.times
+import java.util.*
 
 @Component
 class MatchingService {
 
-	private val sellOrders = PriorityQueue(compareBy(SellLimitOrder::pricePerUnit))
+	private val sellOrders: MutableMap<String, PriorityQueue<SellLimitOrder>> = mutableMapOf()
 
 	fun place(order: SellLimitOrder) {
-		sellOrders.add(order)
+
+		val stockSellOrders = getSellOrdersForStock(order.stock)
+
+		stockSellOrders.add(order)
+	}
+
+	private fun getSellOrdersForStock(stock: String) =
+		when (val existingQueue = sellOrders.get(stock)) {
+			null -> {
+				createNewStockQueue().also {
+					sellOrders[stock] = it
+				}
+			}
+			else -> existingQueue
+		}
+
+	private fun createNewStockQueue(): PriorityQueue<SellLimitOrder> {
+		return PriorityQueue(compareBy(SellLimitOrder::pricePerUnit))
 	}
 
 	fun place(order: BuyMarketOrder): PlaceBuyOrderResult {
-		val match = sellOrders.find {
-			it.stock == order.stock
-		}
+
+		val match = getSellOrdersForStock(order.stock).firstOrNull()
 
 		return if (match != null) {
 			if (order.liquidity < BigDecimal(order.quantity) * match.pricePerUnit) {
