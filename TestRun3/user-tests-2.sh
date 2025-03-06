aa8137cf195697b4c668ae3062c3f1e04cabe4e9
Brent Machado <brentmachado@seng468-group6.rcs.uvic.ca>
2025-03-06 11:16:04 -0800

    Added bash script to run tests

---
+++ TestRun3/user-tests-2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Reseting Containers"
+docker compose down -v
+docker compose up -d
+
+echo "Waiting Until Containers Set Up"
+sleep 10
+
+rm -f ./Config/stockIds.csv
+touch ./Config/stockIds.csv
+chmod +rwx ./Config/stockIds.csv
+
+rm -rf results-initial
+rm -rf results-initial.log
+
+echo "Running InitialSetup.jmx"
+jmeter -n -t InitialSetup.jmx -l results-initial.log -e -o ./results-initial
+
+rm -rf results-10000
+rm -f results-10000.log
+
+echo "Running UserThreadTest.jmx"
+jmeter -n -t UserThreadTest.jmx -l results-10000.log -e -o ./results-10000
+
+
+
